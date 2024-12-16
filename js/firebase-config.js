4907b3d49bc6e834a9f118f9b3ecc221ef565441
jburchel <jburchel@gmail.com>
2024-12-16 12:29:39 -0500

    security: update dependencies and Firebase configuration
    - Add Firebase to package.json
    - Update to non-compat Firebase SDK
    - Update Firebase configuration
    - Fix potential security vulnerabilities

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,14 @@
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBtzaibXTCspENsEVaN8XF5DkuizsjxVX4",
-    authDomain: "crossover-people-finder.firebaseapp.com",
-    databaseURL: "https://crossover-people-finder-default-rtdb.firebaseio.com",
-    projectId: "crossover-people-finder",
-    storageBucket: "crossover-people-finder.firebasestorage.app",
-    messagingSenderId: "35563852058",
-    appId: "1:35563852058:web:a4b89c5f0fedd06432dca3"
+    apiKey: "AIzaSyBrECDgxvQDqtcKXwRQJOmvJbEFCVxHJZA",
+    authDomain: "frontier-finder-c7c0f.firebaseapp.com",
+    projectId: "frontier-finder-c7c0f",
+    storageBucket: "frontier-finder-c7c0f.appspot.com",
+    messagingSenderId: "1012474326796",
+    appId: "1:1012474326796:web:e2a3c9b8f9e0d9f7d5c3a8"
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
