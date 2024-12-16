ecac29b07767bd184fd47e017591162a86ee9015
jburchel <jburchel@gmail.com>
2024-12-16 12:11:39 -0500

    updated firebase config and secrets in github

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,12 @@
 // Firebase configuration
 const firebaseConfig = {
-    // TODO: Replace with your Firebase project configuration
-    apiKey: "AIzaSyBtzaibXTCspENsEVaN8XF5DkuizsjxVX4",
-    authDomain: "crossover-people-finder.firebaseapp.com",
-    databaseURL: "https://crossover-people-finder-default-rtdb.firebaseio.com",
-    projectId: "crossover-people-finder",
-    storageBucket: "crossover-people-finder.firebasestorage.app",
-    messagingSenderId: "35563852058",
-    appId: "1:35563852058:web:a4b89c5f0fedd06432dca3"
+    apiKey: process.env.FIREBASE_API_KEY || "AIzaSyBtzaibXTCspENsEVaN8XF5DkuizsjxVX4",
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN || "crossover-people-finder.firebaseapp.com",
+    databaseURL: process.env.FIREBASE_DATABASE_URL || "https://crossover-people-finder-default-rtdb.firebaseio.com",
+    projectId: process.env.FIREBASE_PROJECT_ID || "crossover-people-finder",
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "crossover-people-finder.firebasestorage.app",
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "35563852058",
+    appId: process.env.FIREBASE_APP_ID || "1:35563852058:web:a4b89c5f0fedd06432dca3"
 };
 
 // Initialize Firebase
